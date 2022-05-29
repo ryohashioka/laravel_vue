115381e738364277a0d03183be374719b729c7ea
ryohashioka <ryo.hashioka@gmail.com>
2022-05-29 09:45:39 +0900

    dynamic-jpg 追加

--- vue/src/components/pages/DynamicImgs.vue
+++ vue/src/components/pages/DynamicImgs.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import DynamicPng from '../atoms/DynamicPng.vue'
+import DynamicJpg from '../atoms/DynamicJpg.vue'
 import images from '../../assets/json/images.json'
 </script>
 
@@ -8,7 +9,9 @@ import images from '../../assets/json/images.json'
     <h1>Dynamic Image Demo</h1>
     <div class="mt-5 flex gap-10">
       <div v-for="image in images">
-        <dynamic-png class="w-full" :filename="image" />
+        <dynamic-png v-if="image.type==='png'" class="w-full" :filename="image.name" />
+        <dynamic-jpg v-else-if="image.type==='jpg'" class="w-full" :filename="image.name" />
+        <p>{{ image.description }}</p>
       </div>
     </div>
   </div>
